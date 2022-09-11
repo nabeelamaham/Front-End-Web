5460f6e17f5dd87bda9288eebf87d7055a98b75d
Nabeela Maham <nabeela.maham@stud.fra-uas.de>
2022-09-11 10:09:03 +0200

    React

--- ReactJSCourseContant/hello-world/src/App.js
+++ ReactJSCourseContant/hello-world/src/App.js
@@ -3,13 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import {Greet} from './components/Greet.js';
 //import Greet from './components/Greet.js';
-
+import Welcome from './components/welcome';
 
 class App extends Component {
 render(){  
   return (
     <div className="App">
   <Greet />
+  <Welcome />
     </div>
   );
 }
