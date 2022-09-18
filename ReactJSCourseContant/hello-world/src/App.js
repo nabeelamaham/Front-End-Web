8aa77b6c7180fc7a184208ae68602918cba8f621
Nabeela Maham <nabeela.maham@stud.fra-uas.de>
2022-09-18 10:04:03 +0200

    error handling, memo comp,parent,portal,reference

--- ReactJSCourseContant/hello-world/src/App.js
+++ ReactJSCourseContant/hello-world/src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './App.css';
+import PortalDemo from './components/PortalDemo';
 //import {Greet} from './components/Greet.js';
 /* import Greet from './components/Greet.js';
 import Welcome from './components/welcome';
@@ -16,18 +17,40 @@ import StyleSheet from './components/StyleSheet'
 import Inline from './components/Inline';
 import './appStyle.css'
 import styles from './appStyles.module.css'
- */
 import Lifecycle from './components/lifecycle';
 import Form from './components/Form'
 import FragmentDemo from './components/FragmentDemo'
 import Table from './components/Table';
 import ParentComp from './components/ParentComp';
+import RefsDemo from './components/RefsDemo';
+import FocusInput from './components/FocusInput';
+*/
+import FRParentInput from './FRParentInput';
+import Hero from './components/Hero';
+import ErrorBoundary from './components/ErrorBoundary';
+import ClickCounter from './components/ClickCounter';
 class App extends Component {
 render(){  
   return (
- <div className="App">
-<ParentComp />
 
+
+ <div className="App">
+  <ClickCounter/>
+  {/* 
+  <ErrorBoundary>  
+  <Hero heroName="Batman" ></Hero>
+  </ErrorBoundary>
+  <ErrorBoundary>  
+  <Hero heroName="Superman" ></Hero>
+  </ErrorBoundary>
+  <ErrorBoundary>  
+  <Hero heroName="Joker" ></Hero>
+  </ErrorBoundary>
+<PortalDemo />
+  <FRParentInput/>
+    <FocusInput/>
+<RefsDemo/>
+ <ParentComp /> */}
 {/* <Table />
 <FragmentDemo />
 <Lifecycle/>   
